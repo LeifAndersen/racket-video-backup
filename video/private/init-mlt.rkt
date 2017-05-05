737cf1166e6e34ddc7c17bd35644781592867449
Leif Andersen <leif@leifandersen.net>
2017-05-05 10:25:15 -0400

    daf

--- video/private/init-mlt.rkt
+++ video/private/init-mlt.rkt
@@ -4,8 +4,6 @@
 ;; Alternatively your program can just call `mlt-factory-init` on its own.
 ;;   If you do this though, do make sure to shut things down with mlt-factory-init
 
-(provide mlt-executor
-         register-mlt-close)
 (require ffi/unsafe
          "mlt.rkt")
 
@@ -27,18 +25,3 @@
     (current-plumber) (λ (p)
                         (collect-garbage)
                         (mlt-factory-close)))))
-
-;; Set up GC thread for MLT objects
-(define mlt-executor (make-will-executor))
-(void
- (thread
-  (λ ()
-    (let loop ()
-      (will-execute mlt-executor)
-      (loop)))))
-;; Register an mlt-object to be closed on garbadge collection
-;; For convience, return the mlt-object when done
-;; (_mlt-properties -> any) _mlt-properties -> _mlt-properties
-(define (register-mlt-close proc v)
-  (will-register mlt-executor v (λ (v) (proc v)))
-  v)
