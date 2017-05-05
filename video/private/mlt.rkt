469dc8da3428f3bb0fce3f845863a4f2aa6a374f
Leif Andersen <leif@leifandersen.net>
2017-05-05 10:20:32 -0400

    adkfjak

--- video/private/mlt.rkt
+++ video/private/mlt.rkt
@@ -2,8 +2,6 @@
 
 (require ffi/unsafe
          ffi/unsafe/define
-         racket/stxparam
-         racket/splicing
          (for-syntax ffi/unsafe
                      racket/syntax
                      racket/base
@@ -19,58 +17,18 @@
                          #:fail (λ () #f)))
 (define-for-syntax mlt-lib (ffi-lib "libmlt" '("6")
                                     #:fail (λ () #f)))
-(define-ffi-definer define-mlt/internal mlt-lib)
-(define-syntax (define-mlt stx)
-  (syntax-parse stx
-    [(_ name:id args ...)
-     (if mlt-lib
-         #'(define-mlt/internal name args ...)
-         #'(define (name . rst) (raise-user-error 'video "MLT Not Installed")))]))
-
-(define-syntax-parameter current-func-name #f)
-(define-syntax (define-mlt* stx)
-  (syntax-parse stx
-    [(_ name:id args ...)
-     #:with c-name (format-id stx "~a" (string-replace (symbol->string (syntax-e #'name)) "-" "_"))
-     (syntax/loc stx
-       (splicing-syntax-parameterize ([current-func-name (make-rename-transformer #'name)])
-         (define-mlt name args ...
-           #:c-id c-name)))]))
-
-;; MLT Error
-(struct exn:fail:mlt exn:fail ())
-(struct exn:fail:mlt:warning exn:fail:mlt ())
-(define raise-mlt-warnings (make-parameter #f))
-(define-syntax (raise-mlt-warning stx)
-  (syntax-parse stx
-    [(_ function:id)
-     #:with expanded-function (local-expand #'function 'expression #f)
-     (syntax/loc stx
-       (when (raise-mlt-warnings)
-         (raise (exn:fail:mlt:warning (format "MLT: Function ~a returned warning" 'expanded-function)
-                                      (current-continuation-marks)))))]))
-(define-syntax (raise-mlt-error stx)
-  (syntax-parse stx
-    [(_ function:id (~optional (~seq #:msg msg:str)
-                               #:defaults ([msg #'""])))
-     #:with expanded-function (local-expand #'function 'expression #f)
-     (syntax/loc stx
-       (raise (exn:fail:mlt (format "MLT: Function ~a failed: ~a" 'expanded-function msg)
-                            (current-continuation-marks))))]))
-(define-syntax (null-error stx)
-  (syntax-parse stx
-    [(_ v)
-     (quasisyntax/loc stx (or v (raise-mlt-error current-func-name)))]))
+(define-ffi-definer define-mlt mlt-lib)
 
 ;; Types
 (define-cpointer-type _mlt-repository)
 (define-cpointer-type _mlt-profile-pointer)
 
 ;; Factory
-(define-mlt* mlt-factory-init (_fun _path -> [v : _mlt-repository/null]
-                                    -> (null-error v)))
-(define-mlt* mlt-factory-close (_fun -> _void))
+(define-mlt mlt-factory-init (_fun _path -> _mlt-repository/null)
+  #:c-id mlt_factory_init)
+(define-mlt mlt-factory-close (_fun -> _void)
+  #:c-id mlt_factory_close)
 
 ;; Profile
-(define-mlt* mlt-profile-init (_fun _string -> [v : _mlt-profile-pointer/null]
-                                    -> (null-error v)))
+(define-mlt mlt-profile-init (_fun _string -> [v : _mlt-profile-pointer/null])
+  #:c-id mlt_profile_init)
